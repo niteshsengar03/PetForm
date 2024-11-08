c5656f7c54c8fd6753fa26c2e943c210c264f2c8
Nitesh <niteshsengar9831@gmail.com>
2024-11-09 03:11:31 +0530

    Initailise the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    <div className="flex justify-center">
+      <Button>Click me</Button>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
